d65b55ea5a91a68b9f8cd9d32885ced914cc0a28
rohit1039 <rohitparida0599@gmail.com>
2023-03-06 23:20:25 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'flickity/dist/flickity.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "flickity/dist/flickity.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  )
 );
